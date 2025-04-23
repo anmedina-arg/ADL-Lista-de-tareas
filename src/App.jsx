1a281aa2f87a333d3c7091dc1fc663f758935c8c
anmedina-arg <andres.medina.arg@gmail.com>
2025-04-23 20:44:47 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Tareas from './components/Tareas';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Tareas />
+    </>
+  );
+}
+
+export default App;
---
+++ src/mockData/TareasIniciales.js
@@ -0,0 +1,14 @@
+export const tareasIniciales = [
+    {
+      nombre: "Tarea uno",
+      completada: false,
+    },
+    {
+      nombre: "Tarea dos",
+      completada: false,
+    },
+    {
+      nombre: "Tarea tres",
+      completada: true,
+    }
+]
